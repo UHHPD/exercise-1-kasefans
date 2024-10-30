b80e37a3207b985649de0dd3c7df197190bc0755
SchroederRoberta <139751009+SchroederRoberta@users.noreply.github.com>
2024-10-30 15:21:33 +0000

    Aufgabe 2 including terminal output.

--- summieren.cc
+++ summieren.cc
@@ -1,19 +1,33 @@
+#include <iostream>
 #include <fstream>
 
-// Open file and read out summands. Close file.
-std::ifstream fin("daten.txt");
+int main(){
+  // Open file and read out summands.
+  std::ifstream fin("daten.txt");
 
-// Loop over all lines in file.
-int summand_1, summand_2;
-for(int i = 0; i < 234; i++){
-  
+  // Open output file.
+  std::ofstream fout("datensumme.txt");
+
+  // Loop over all lines in file.
+  int summand_1, summand_2;
+  for(int i = 0 ; i < 234; i++){
+    fin >> summand_1 >> summand_2;
+
+    // Write summands to the terminal.
+    std::cout << "\nSummand 1: " << summand_1 << "\nSummand 2: " << summand_2 << std::endl;
+
+    // Sum summands.
+    int _sum = summand_1 + summand_2;
+
+    // Write sum to the terminal.
+    std::cout << "Sum: " << _sum << std::endl;
+
+    // Write sum to file.
+    fout << _sum << std::endl;
+  }
+
+  // Close files.
+  fin.close();
+  fout.close();
+
 }
-
-fin >> summand_1 >> summand_2;
-fin.close();
-
-// Write summands to the terminal.
-std::cout << "Summand 1: " << summand_1 << "Summand 2: " << summand_2 << std::endl;
-
-// Sum summands.
-int _sum = summand_1 + summand_2;
